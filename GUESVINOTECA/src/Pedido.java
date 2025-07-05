12ab8d0e98eba67efda9bfb275141a63a9d1208d
franrg19 <franrg19@outlook.com>
2025-07-05 12:14:33 +0200

    Comienzo del proyecto,creando clases principales

---
+++ GUESVINOTECA/src/Pedido.java
@@ -0,0 +1,53 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Pedido {
+    //Atributos
+    private Cliente cliente;
+    private ArrayList<Vino> lineasDeVenta;
+    private ArrayList<Integer> cantidades;
+    private LocalDate fecha;
+    private double totalVenta;
+
+    //Constructor
+    public Pedido(Cliente cliente,LocalDate fecha) {
+        this.cliente = cliente;
+        this.lineasDeVenta = new ArrayList<>();
+        this.fecha=fecha;
+        this.totalVenta = 0.0;
+    }
+    //Metodo para añadir vino
+    public void añadirProducto(Vino vino, int cantidad){
+        this.lineasDeVenta.add(vino);
+        this.totalVenta += vino.getPrecio() * cantidad;
+    }
+
+    //Getters
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+    public ArrayList<Vino> getLineasDeVenta() {
+        return lineasDeVenta;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public double getTotalVenta() {
+        return totalVenta;
+    }
+
+    @Override
+    public String toString() {
+        return "Pedido{" +
+                "cliente=" + cliente +
+                ", lineasDeVenta=" + lineasDeVenta +
+                ", fecha=" + fecha +
+                ", totalVenta=" + totalVenta +
+                '}';
+    }
+}
+
+
---
+++ GUESVINOTECA/src/Vino.java
@@ -0,0 +1,68 @@
+public class Vino {
+    //Atributos
+    private String nombre;
+    private String tipo;  // Tinto,Blanco,Rosado,Espumoso
+    private double precio;
+    private int stock;
+
+    //Constructor
+    public Vino(double precio, String tipo, String nombre,int stock) {
+        this.precio = precio;
+        this.tipo = tipo;
+        this.nombre = nombre;
+        this.stock = stock;
+    }
+
+    //Getters y Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public int getStock (){
+        return stock;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setStock(int stock){
+        this.stock=stock;
+    }
+
+
+    //Metodo para decrementar el stock
+    public void decrementarStock(int cantidad){
+        if (this.stock >= cantidad){
+            this.stock -= cantidad;
+        }else {
+            System.out.println("No hay suficiente stock de: " + this.nombre + ".");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Vino{" +
+                "nombre='" + nombre + '\'' +
+                ", tipo='" + tipo + '\'' +
+                ", precio=" + precio + "€" +
+                ", stock=" + stock +
+                '}';
+    }
+}
